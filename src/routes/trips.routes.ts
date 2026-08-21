0873ac7091a8611b99cb8f5d22511f1780524e8b
Maksym Druziuk <druziukmaksym@gmail.com>
2026-08-21 17:53:11 +0200

    add delete Trips button

--- src/routes/trips.routes.ts
+++ src/routes/trips.routes.ts
@@ -14,7 +14,7 @@ router.post('/', requireAuth, validateBody(createTripSchema), createTripHandler)
 router.get('/', requireAuth, listTripsHandler);
 router.get('/:id', requireAuth, getTripHandler);
 router.post('/:id/destinations', requireAuth, validateBody(addDestinationSchema), addDestinationHandler);
-router.delete('/:id', requireAuth, validateBody(deleteTripSchema), deleteTripHandler);
+router.delete('/:id', requireAuth, deleteTripHandler);
 router.patch('/:id/destinations/:destinationId', requireAuth, validateBody(updateAccommodationSchema), updateAccommodationHandler);
 
 export default router;
